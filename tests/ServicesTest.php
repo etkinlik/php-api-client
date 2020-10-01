e0984e438283b2dd9354d45bea1dde269b264702
İrfan Evrens <irfanevrens@gmail.com>
2020-10-01 06:39:37 +0300

    bazı fonksiyon isimlerinde düzeltmeler yapıldı.

--- tests/ServicesTest.php
+++ tests/ServicesTest.php
@@ -1,25 +1,30 @@
 <?php
 
+use Carbon\Carbon;
+use EtkinlikApi\ApiClient;
+use EtkinlikApi\Model\Config\EventsConfig;
+use EtkinlikApi\Model\Config\VenuesConfig;
+
 class ServicesTest extends PHPUnit_Framework_TestCase
 {
     public function testTurler()
     {
-        $client = new \EtkinlikApi\ApiClient('token buraya gelecek');
+        $client = new ApiClient('token buraya gelecek');
 
         $events = $client->event->getItems(
-            (new \EtkinlikApi\Model\Config\EventsConfig())
-                ->setStartGte(\Carbon\Carbon::now()->addDay()->toDateTimeString())
-                ->setEndLte(\Carbon\Carbon::now()->addDay(4)->toDateTimeString())
+            (new EventsConfig())
+                ->setStartGte(Carbon::now()->addDay()->toDateTimeString())
+                ->setEndLte(Carbon::now()->addDay(4)->toDateTimeString())
                 ->addCategoryId(4015)
         );
 
         $event = $client->event->getById(104289);
 
         $formats = $client->format->getItems();
-        $categories = $client->category->getListe();
+        $categories = $client->category->getItems();
 
         $venues = $client->venue->getItems(
-            (new \EtkinlikApi\Model\Config\VenuesConfig())
+            (new VenuesConfig())
                 ->addCityId(7)
         );
 
